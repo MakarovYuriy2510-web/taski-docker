2bd9357e3e2fd7c653db030dcc3093234dfdbf74
Макаров Юрий Олегович <MakarovYuriy1987@yandex.ru>
2026-04-28 14:35:23 +0300

    v2

---
+++ backend/api/tests.py
@@ -0,0 +1,28 @@
+# backend/api/tests.py
+"""Проверка создания задачи."""
+
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+from api import models
+
+
+class TaskiAPITestCase(TestCase):
+    """Проверка создания задачи."""
+
+    def setUp(self):
+        """Проверка создания задачи."""
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
